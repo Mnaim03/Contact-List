cd8b8dd1bc07737d8089481f2e2ce5424712de10
Vincenzo Ragone <vincemaiello04@gmail.com>
2024-11-21 13:31:57 +0100

    Delete

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.demo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.main.it.university.group27 to javafx.fxml;
-    exports org.main.it.university.group27;
-}
