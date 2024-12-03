861336c40fe742f65ac6efa55d1ca9a38b672a63
Mnaim03 <99801017+Mnaim03@users.noreply.github.com>
2024-12-03 10:32:14 +0100

    first css commit

--- SWEDEMO/src/main/java/com/example/NewMain.java
+++ SWEDEMO/src/main/java/com/example/NewMain.java
@@ -7,6 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.net.URL;
 
 
 public class NewMain extends Application{
@@ -15,9 +16,14 @@ public class NewMain extends Application{
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/scene1.fxml"));
- 
-        
+
         Scene scene = new Scene(root);
+
+        //css file addiction
+        URL url = this.getClass().getResource("/fxml/style.css");
+        String css = url.toExternalForm();
+        scene.getStylesheets().add(css);
+
         stage.setScene(scene);
         stage.show();
     }
