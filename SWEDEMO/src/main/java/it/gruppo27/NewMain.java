ec500da227181f808008f1e78de96490394de630
Mnaim03 <99801017+Mnaim03@users.noreply.github.com>
2024-12-09 11:32:36 +0100

    Scene Update
    Co-Authored-By: zolvyo <45299201+frncy01@users.noreply.github.com>
    Co-Authored-By: Vincenzo Ragone  <137784339+vincenzo113@users.noreply.github.com>

--- SWEDEMO/src/main/java/it/gruppo27/NewMain.java
+++ SWEDEMO/src/main/java/it/gruppo27/NewMain.java
@@ -1,40 +1,33 @@
 package it.gruppo27;
-
 import javafx.application.Application;
-import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.net.URL;
 
+public class NewMain extends Application {
 
-public class NewMain extends Application{
 
-    
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/scene1.fxml"));
-
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/scene.fxml"));
         Scene scene = new Scene(root);
 
-        //css file addiction
-        URL url = this.getClass().getResource("/fxml/style.css");
+        // CSS file addition
+        URL url = this.getClass().getResource("/fxml/cupertino-dark.css");
         String css = url.toExternalForm();
         scene.getStylesheets().add(css);
 
         stage.setScene(scene);
         stage.show();
     }
-    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
-    
 
